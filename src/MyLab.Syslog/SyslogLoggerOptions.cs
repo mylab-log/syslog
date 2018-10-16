66dd671dbead5c68578d1897be01371cf1d54d07
Andrey O <ozzy.ext@gmail.com>
2018-10-17 02:29:13 +0300

    Finally implementation

---
+++ src/MyLab.Syslog/SyslogLoggerOptions.cs
@@ -0,0 +1,39 @@
+﻿namespace MyLab.Syslog
+{
+    /// <summary>
+    /// Contains syslog writing options
+    /// </summary>
+    public class SyslogLoggerOptions
+    {
+        /// <summary>
+        /// Overrides default hostname
+        /// </summary>
+        public string Hostname { get; set; }
+
+        /// <summary>
+        /// Overrides current application name
+        /// </summary>
+        public string AppName { get; set; }
+
+        /// <summary>
+        /// Overrides default process id. Should be generated value.
+        /// </summary>
+        public string ProcId { get; set; }
+
+        /// <summary>
+        /// Gets syslog host
+        /// </summary>
+        public string RemoteHost { get; set; }
+
+        /// <summary>
+        /// Gets syslog port
+        /// </summary>
+        public int RemotePort { get; set; }
+
+        /// <summary>
+        /// Gets facility. Local7 - by default.
+        /// </summary>
+        /// <remarks>https://tools.ietf.org/html/rfc5424#section-6.2.1</remarks>
+        public int Facility { get; set; } = 23;
+    }
+}
---
+++ src/MyLab.Syslog/SyslogLoggerProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace MyLab.Syslog
+{
+    [ProviderAlias("syslog")]
+    class SyslogLoggerProvider : ILoggerProvider
+    {
+        public SyslogLoggerOptions Options { get; }
+
+        public SyslogLoggerProvider()
+        {
+            
+        }
+
+        public SyslogLoggerProvider(IOptions<SyslogLoggerOptions> options)
+            : this(options.Value)
+        {
+            
+        }
+
+        public SyslogLoggerProvider(SyslogLoggerOptions options)
+        {
+            Options = options;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new SyslogLogger(new UdpLogSenderFactory(), Options);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
